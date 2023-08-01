2dfdaefb40d9ccaf1f08b39cc6fa3904d47f6b82
Guilherme Lourenco <49289593+lourencoguilherme@users.noreply.github.com>
2023-08-01 18:07:34 -0300

    Update flyway-migrate-db.sh

--- resources/flyway-migrate-db.sh
+++ resources/flyway-migrate-db.sh
@@ -6,6 +6,6 @@ USER=$3
 PASSWORD=$4
 
 docker run --rm \
--v "${PWD}"/db/migration/production/:/flyway/sql \
+-v ./db/migration/production/:/flyway/sql \
 flyway/flyway:9.18.0-alpine -user=${USER} -password=${PASSWORD} -baselineOnMigrate=false -outOfOrder=true -sqlMigrationPrefix=V migrate \
 -url="jdbc:postgresql://${HOST}/${DATABASE}"
