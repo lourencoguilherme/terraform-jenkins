163c4a8cf007c9e6364f6dd3c1699f8424204d71
Guilherme Lourenco <guilherme.lourenco@docket.com.br>
2023-08-02 18:15:31 -0300

    Change script

--- resources/db/flyway-migrate-db.sh
+++ resources/db/flyway-migrate-db.sh
@@ -5,11 +5,10 @@ HOST=$2
 DATABASE=$3
 USER=$4
 PASSWORD=$5
-WORKSPACE=$6
 
 docker-compose run --rm \
 flyway \
--v "${WORKSPACE}"/db/migration/:/flyway/sql \
+-v "${PWD}"/db/migration/:/flyway/sql \
 -user=${USER} -password=${PASSWORD} \
 -baselineOnMigrate=false -outOfOrder=true -sqlMigrationPrefix=V \
 -url="jdbc:postgresql://${HOST}/${DATABASE}" \
