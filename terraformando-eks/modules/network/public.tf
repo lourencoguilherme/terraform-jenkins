617af40722ad9a40432e333331a0ce7644e6553f
Guilherme Lourenco <guilherme.lourenco@docket.com.br>
2023-08-11 14:39:25 -0300

    feat: create public router table and subnet associations

--- terraformando-eks/modules/network/public.tf
+++ terraformando-eks/modules/network/public.tf
@@ -21,3 +21,13 @@ resource "aws_subnet" "public_subnet_1c" {
     "Name" = format("%s-public-1c", var.cluster_name)
   }
 }
+
+resource "aws_route_table_association" "public_1a" {
+  subnet_id = aws_subnet.public_subnet_1a.id
+  route_table_id = aws_route_table.igw_route_table.id
+}
+
+resource "aws_route_table_association" "public_1c" {
+  subnet_id = aws_subnet.public_subnet_1c.id
+  route_table_id = aws_route_table.igw_route_table.id
+}
