a1274d3d0ca38ef368bb0fd97b35a0a8fd4647ba
Guilherme Lourenco <guilherme.lourenco@docket.com.br>
2023-09-28 12:00:25 -0300

    chore: cypress disable video

--- services-test/cypress-e2e/cypress.config.js
+++ services-test/cypress-e2e/cypress.config.js
@@ -1,16 +1,40 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  viewportHeight: 1080,
-  viewportWidth: 1920,
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    chromeWebSecurity: false,
+    retries: 2,
+    defaultCommandTimeout: 5000,
+    watchForFileChanges: false,
+    videosFolder: 'tests/cypress/videos',
+    screenshotsFolder: 'tests/cypress/screenshots',
+    fixturesFolder: 'tests/cypress/fixture',
+    e2e: {
+        setupNodeEvents(on, config) {
+            on('before:browser:launch', (browser = {}, launchOptions) => {
+                console.log(launchOptions.args)
+
+                if (browser.family == 'chromium') {
+                    launchOptions.args.push('--disable-gpu')
+                }
+
+                return launchOptions
+            });
+            return require('./tests/cypress/plugins/index.js')(on, config)
+        },
+        baseUrl: 'http://wedleague.loc',
+        specPattern: 'tests/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
+        supportFile: 'tests/cypress/support/index.js',
     },
-    video: false,
-    env: {
-        ELECTRON_DISABLE_GPU: "1",
-        ELECTRON_EXTRA_LAUNCH_ARGS: '--disable-gpu'
-    }
-  },
-});
+})
+
+module.exports = (on, config) => {
+    on('before:browser:launch', (browser = {}, launchOptions) => {
+        console.log(launchOptions.args)
+
+        if (browser.family == 'chromium') {
+            launchOptions.args.push('--disable-gpu')
+        }
+
+        return launchOptions
+    });
+}
